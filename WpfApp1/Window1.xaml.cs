0b41a8757d9f80e9e58ab8eac5958a23585cd858
Daria9898 <kellw9@mail.ru>
2024-11-18 18:15:49 +1000

    Add logic

--- WpfApp1/Window1.xaml.cs
+++ WpfApp1/Window1.xaml.cs
@@ -17,18 +17,73 @@ namespace WpfApp1
     /// <summary>
     /// Логика взаимодействия для Window1.xaml
     /// </summary>
-    public partial class Window1 : Window
+    public partial class Window1
     {
+        private double _currentValue = 0;
+        private double _storedValue = 0;
+        private char _operation = ' ';
         public Window1()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow window = new MainWindow();
-            window.Show();
-            this.Close();
+            Button button = (Button)sender;
+            string number = button.Content.ToString();
+            if (txtDisplay.Text == "0" && number != ".")
+            {
+                txtDisplay.Text = number;
+            }
+            else
+            {
+                txtDisplay.Text += number;  
+            }
+        }
+
+        private void OperationButton_Click(Object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            _operation = button.Content.ToString()[0];
+            _storedValue = double.Parse(txtDisplay.Text);
+            txtDisplay.Text = "0";
+        }
+
+        private void EqualsButton_Click(this object sender, RoutedEventArgs e)
+        {
+            _currentValue = double.Parse(txtDisplay.Text);
+            switch( _operation )
+            {
+                case '+':
+                    _currentValue = _storedValue + _currentValue;
+                    break;
+                case '-':
+                    _currentValue = _storedValue - _currentValue;
+                    break;  
+                case '*':
+                    _currentValue = _storedValue * _currentValue;
+                    break;
+                case '/':
+                    if( _storedValue != 0)
+                    {
+                        _currentValue = _storedValue / _currentValue;
+                    }
+                    else
+                    {
+                        MessageBox.Show("На 0 делить нельзя!!");
+                    }
+                    break;
+            }
+            txtDisplay.Text = _currentValue.ToString();
+            _operation = '\0';
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text = "0";
+            _currentValue = 0;
+            _storedValue = 0;
+            _operation = '\0';
         }
     }
 }
